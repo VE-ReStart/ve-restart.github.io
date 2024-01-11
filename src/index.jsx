e9b71147cd92cb07542352118765facc53d8157a
uwuelias <eliaswu466@gmail.com>
2024-01-11 10:53:48 -0500

    fix for gh pages pt 2

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from './routes/Home'
 import Product from './routes/Products'
