29dacdd97e9b2a65e33c509096baf9c1f92d70b1
uwuelias <eliaswu466@gmail.com>
2024-02-02 10:45:29 -0500

    added promotion section

---
+++ src/routes/Home/PromotionSection.jsx
@@ -0,0 +1,11 @@
+function PromotionSection() {
+	return (
+		<section id='promotion'>
+			<p id='promotionText'>
+				VALENTINE&apos;S DAY DEAL: BUY 1 GET 1 14% OFF WITH CODE ILOVEU
+			</p>
+		</section>
+	)
+}
+export default PromotionSection
+
